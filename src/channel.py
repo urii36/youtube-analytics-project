0ff615916cdbe8e1d3bba332e8a08ae43e46d5a4
urii36 <kazan313131@gmail.com>
2023-06-29 15:02:28 +0300

    home work_3

--- src/channel.py
+++ src/channel.py
@@ -2,8 +2,10 @@ import json
 import os
 
 from googleapiclient.discovery import build
+from functools import total_ordering
 
 
+@total_ordering
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YT_API_KEY')
@@ -16,9 +18,24 @@ class Channel:
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
         self.url = f'https://www.youtube.com/channel/{self.channel_id}'
-        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __eq__(self, other):
+        return self.subscriber_count == other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
 
     @classmethod
     def get_service(cls):
