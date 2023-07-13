cd0f9cd678aab9157671aee7049a7b222e4d32f9
urii36 <kazan313131@gmail.com>
2023-07-13 16:21:47 +0300

    home work_5/1 YT

--- homework-5/main.py
+++ homework-5/main.py
@@ -1,11 +1,11 @@
 import datetime
 
-from src.playlist import PlayList
+from src.video import PlayList
 
 if __name__ == '__main__':
     pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
-    assert pl.title == "Moscow Python Meetup №81"
-    assert pl.url == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
+    assert pl.title_list == "Moscow Python Meetup №81"
+    assert pl.url_list == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
 
     duration = pl.total_duration
     assert str(duration) == "1:49:52"
